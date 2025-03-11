6f9fe1d24f5c31f64fa090118411342d58929f43
Kacper Bondaruk <kacperbondarukpython@gmail.com>
2025-03-11 20:53:00 +0100

    - added home gallery - fixed images styling - changed title header navbar

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from .views import DashboardView, HomeView, my_custom_login_view
 urlpatterns = [
     path("dashboard/", DashboardView.as_view(), name="dashboard"),
     path('login/', my_custom_login_view, name='login'),
+    path('', HomeView.as_view(), name='home'),
 ]
